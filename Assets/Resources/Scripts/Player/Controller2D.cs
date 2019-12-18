95c9ed65ad84bb8174e59ff78b6b03432886b5bc
caioguedesam <caioguedesam@gmail.com>
2019-12-17 23:59:29 -0300

    Adding raycast movement and slope climb - 17/12

---
+++ Assets/Resources/Scripts/Player/Controller2D.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+    // Skin width to fire rays from within object
+    const float skinWidth = 0.015f;
+    // Number of rays in each axis
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+    // Layers to collide with
+    public LayerMask collisionMask;
+    // Max angle able to climb slope
+    public float maxClimbAngle = 80f;
+
+    // Collider
+    private BoxCollider2D coll;
+    // Ray origins
+    private RaycastOrigins raycastOrigins;
+    // Object collision information
+    public CollisionInfo collisions;
+
+    // Space between collision raycasts
+    private float horizontalRaySpacing, verticalRaySpacing;
+
+    private void Awake()
+    {
+        // Setting references
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    private void Start()
+    {
+        CalculateRaySpacing();
+        collisions.Reset();
+    }
+
+    /// <summary>
+    /// Updates Raycast Origins based on current collider bounds and skin width.
+    /// </summary>
+    private void UpdateRaycastOrigins()
+    {
+        Bounds bounds = coll.bounds;
+        bounds.Expand(-2 * skinWidth);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    /// <summary>
+    /// Calculates collision raycast spacing based on ray count and collider size.
+    /// </summary>
+    private void CalculateRaySpacing()
+    {
+        Bounds bounds = coll.bounds;
+        bounds.Expand(-2 * skinWidth);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    public void Move(Vector3 moveAmount)
+    {
+        // Recalculate ray origins and collision data every move call
+        UpdateRaycastOrigins();
+        collisions.Reset();
+
+        // Get collision information based on move amount
+        if (moveAmount.x != 0)
+        {
+            HorizontalCollisions(ref moveAmount);
+        }
+        if(moveAmount.y != 0)
+        {
+            VerticalCollisions(ref moveAmount);
+        }
+
+        // Move based on move amount
+        transform.Translate(moveAmount);
+    }
+
+    /// <summary>
+    /// Changes the movement amount based on object's horizontal collisions.
+    /// </summary>
+    /// <param name="moveAmount">Movement amount to be changed.</param>
+    private void HorizontalCollisions(ref Vector3 moveAmount)
+    {
+        // Movement direction (-1 down, 1 up)
+        float directionX = Mathf.Sign(moveAmount.x);
+        // Length of ray
+        float rayLength = Mathf.Abs(moveAmount.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            // Ray origin on bottom left if moving down, top left if moving up
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            // Ray origin updated each iteration (also vertical rays calculated after x movement amount)
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+
+            // Cast the given ray
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            // If ray hits something,
+            if (hit)
+            {
+                // Getting collision angle
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                // If angle is climbable, climb
+                if (i == 0 && slopeAngle <= maxClimbAngle)
+                {
+                    float distanceToSlopeStart = 0f;
+                    // If starting to climb a new slope, move distance to
+                    // slope start (since ray collides before movement is done)
+                    if(slopeAngle != collisions.slopeAnglePast)
+                    {
+                        distanceToSlopeStart = hit.distance - skinWidth;
+                        moveAmount.x -= distanceToSlopeStart * directionX;
+                    }
+                    ClimbSlope(ref moveAmount, slopeAngle);
+                    // Adding move distance to slope start back
+                    moveAmount.x += distanceToSlopeStart * directionX;
+                }
+
+                // Only check other rays after first if not climbing slope
+                // or if slope is too steep.
+                if(!collisions.climbingSlope || slopeAngle > maxClimbAngle)
+                {
+                    // Next update x movement will be only the distance between object and ray collided target
+                    moveAmount.x = (hit.distance - skinWidth) * directionX;
+                    // Changes ray distance to not hit objects further away from foremost hit
+                    rayLength = hit.distance;
+
+                    // If slope angle is too steep, y move amount must be set as well
+                    if (collisions.climbingSlope)
+                    {
+                        // Setting y move amount following y = tan(angle) * move X (set AFTER slope climb)
+                        moveAmount.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(moveAmount.x);
+                    }
+
+                    // Set collision information depending on direction
+                    collisions.left = (directionX == -1);
+                    collisions.right = (directionX == 1);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Changes the movement amount based on object's vertical collisions.
+    /// </summary>
+    /// <param name="moveAmount">Movement amount to be changed.</param>
+    private void VerticalCollisions(ref Vector3 moveAmount)
+    {
+        // Movement direction (-1 down, 1 up)
+        float directionY = Mathf.Sign(moveAmount.y);
+        // Length of ray
+        float rayLength = Mathf.Abs(moveAmount.y) + skinWidth;
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            // Ray origin on bottom left if moving down, top left if moving up
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            // Ray origin updated each iteration (also vertical rays calculated after x movement amount)
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + moveAmount.x);
+            
+            // Cast the given ray
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.magenta);
+
+            // If ray hits something,
+            if (hit)
+            {
+                // Next update y movement will be only the distance between object and ray collided target
+                moveAmount.y = (hit.distance - skinWidth) * directionY;
+                // Changes ray distance to not hit objects further away from foremost hit
+                rayLength = hit.distance;
+
+                // If climbing slope and vertical collision happens, x needs to be set
+                if (collisions.climbingSlope)
+                {
+                    moveAmount.x = moveAmount.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(moveAmount.x);
+                }
+
+                // Set collision information depending on direction (only after slope check)
+                collisions.below = (directionY == -1);
+                collisions.above = (directionY == 1);
+            }
+        }
+    }
+
+    public void ClimbSlope(ref Vector3 moveAmount, float slopeAngle)
+    {
+        // The X original move amount is the new full move amount on slope;
+        // Calculating new X and Y movement based on this and slope angle.
+        float moveDistance = Mathf.Abs(moveAmount.x);
+        // y = sin(angle) * move amount (moveAmount.x)
+        float slopeYMoveAmount = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+        // x = cos(angle) * move amount (moveAmount.x)
+        // on X calculation, keep the sign to indicate direction of slope movement
+        float slopeXMoveAmount = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(moveAmount.x);
+
+        if (moveAmount.y <= slopeYMoveAmount)
+        {
+            moveAmount.y = slopeYMoveAmount;
+            moveAmount.x = slopeXMoveAmount;
+            // Since slope is being climbed, object is grounded
+            collisions.below = true;
+            // And is climbing
+            collisions.climbingSlope = true;
+            collisions.slopeAngle = slopeAngle;
+        }
+    }
+
+    public struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight, bottomLeft, bottomRight;
+    }
+
+    [System.Serializable]
+    public struct CollisionInfo
+    {
+        public bool above, below, left, right;
+        public bool climbingSlope;
+        public float slopeAngle, slopeAnglePast;
+
+        public void Reset()
+        {
+            above = below = left = right = climbingSlope = false;
+
+            slopeAnglePast = slopeAngle;
+            slopeAngle = 0f;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class PlayerControl : MonoBehaviour
+{
+    // Input variables
+    private float horizontalInput, verticalInput;
+    public bool jumpInput;
+
+    // Player environment variables
+    public float moveSpeed = 5f;
+    public float jumpHeight = 4f;
+    public float timeToJumpPeak = 0.4f;
+    private float jumpVelocity;
+    private float playerGravity;
+    public float accelerationTimeAirborne = .15f;
+    public float accelerationTimeGrounded = .1f;
+
+    Vector3 moveAmount;
+    private float moveAmountXSmoothing;
+
+    // Player references
+    Controller2D controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<Controller2D>();
+        CalculateJumpVariables();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        jumpInput = Input.GetButtonDown("Jump");
+    }
+
+    private void Jump()
+    {
+        // Allow jump if jump input is pressed and player is grounded
+        if(jumpInput && controller.collisions.below)
+        {
+            moveAmount.y = jumpVelocity;
+        }
+    }
+
+    /// <summary>
+    /// Calculates jump/gravity movement variables based on REAL WORLD PHYSICS!
+    /// </summary>
+    private void CalculateJumpVariables()
+    {
+        playerGravity = (-2 * jumpHeight) / Mathf.Pow(timeToJumpPeak, 2);
+        jumpVelocity = Mathf.Abs(playerGravity) * timeToJumpPeak;
+    }
+
+    private void SmoothXMoveAmount()
+    {
+        float targetMoveAmountX = horizontalInput * moveSpeed;
+        moveAmount.x = Mathf.SmoothDamp(moveAmount.x, targetMoveAmountX, ref moveAmountXSmoothing,
+            (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+    }
+
+    private void Update()
+    {
+        GetInput();
+
+        // Resetting y movement if collided vertically (grounded)
+        if(controller.collisions.above || controller.collisions.below)
+        {
+            moveAmount.y = 0f;
+        }
+
+        Jump();
+
+        //moveAmount.x = horizontalInput * moveSpeed;
+        SmoothXMoveAmount();
+        moveAmount.y += playerGravity * Time.deltaTime;
+        controller.Move(moveAmount * Time.deltaTime);
+    }
+}
