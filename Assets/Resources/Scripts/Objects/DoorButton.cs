3381524d2725d4c71234d3601f85b99d2dfac80c
caioguedesam <caioguedesam@gmail.com>
2020-01-03 15:46:21 -0300

    Trying to implement button rewind. Still needs much work - 03/01/2020

--- Assets/Resources/Scripts/Objects/DoorButton.cs
+++ Assets/Resources/Scripts/Objects/DoorButton.cs
@@ -10,8 +10,16 @@ public class DoorButton : MonoBehaviour
     public List<GameObject> listOfDoors;
     // Is the button timed?
     public bool isTimed = false;
+    public bool timerRunning { get; private set; } = false;
     // Seconds for button timer
     public float buttonTimer = 3f;
+    // Button timer variables
+    public float buttonCurrentTime;
+    private float buttonStartTime = 0f;
+    [SerializeField]
+    private float buttonEndTime = 0f;
+    [SerializeField]
+    private float minButtonTime;
 
     // Delay time for next toggle on button
     public float buttonToggleDelayTime = 0.3f;
@@ -23,6 +31,11 @@ public class DoorButton : MonoBehaviour
         player = GameObject.FindWithTag("Player").GetComponent<PlayerControl>();
     }
 
+    private void Start()
+    {
+        minButtonTime = 0f;
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         // Player pressed button
@@ -32,7 +45,7 @@ public class DoorButton : MonoBehaviour
         else if(collision.CompareTag("TimeClone"))
         {
             Clone clone = collision.GetComponent<Clone>();
-            if(clone.clonePositions[clone.posIndex].input.interactInput)
+            if (clone.clonePositions[clone.posIndex-1].input.interactInput)
             {
                 StartCoroutine(ToggleButton());
             }
@@ -59,13 +72,70 @@ public class DoorButton : MonoBehaviour
             // If the button is timed, wait timer then re-toggle all objects
             if(isTimed)
             {
+                /*timerRunning = true;
+                buttonStartTime = Time.time;
                 yield return new WaitForSeconds(buttonTimer);
                 ActivateAllButtonObjects();
+                timerRunning = false*/
+
+                timerRunning = true;
+                buttonStartTime = Time.time;
+
+                while (buttonCurrentTime < buttonTimer) {
+                    yield return null;
+                }
+                ActivateAllButtonObjects();
+                buttonEndTime = Time.time;
+                timerRunning = false;
             }
             else
                 yield return new WaitForSeconds(buttonToggleDelayTime);
 
             isButtonActivating = false;
         }
+    }
+
+    // HOLY SHIT THIS IS SO BAD REWORK THIS PLEASE
+    private void CalculateButtonCurrentTime()
+    {
+        /*if (!timerRunning && !TimeController.Instance.isRewindingTime)
+        {
+            if (buttonEndTime != 0 && Time.time - buttonEndTime <= TimeController.Instance.rewindSeconds)
+            {
+                Debug.Log(Time.time - buttonEndTime);
+                buttonCurrentTime += Time.deltaTime;
+                minButtonTime = Mathf.Max(minButtonTime, buttonCurrentTime - TimeController.Instance.rewindSeconds);
+                Debug.Log("minbuttontime = " + minButtonTime);
+            }
+            else
+            {
+                buttonCurrentTime = 0f;
+                //minButtonTime = 0f;
+            }
+            
+        }*/
+        if(!timerRunning)
+        {
+            buttonCurrentTime = 0f;
+        }
+        else if(!TimeController.Instance.isRewindingTime && buttonStartTime != 0 && buttonCurrentTime < buttonTimer)
+        {
+            //buttonCurrentTime = Time.time - buttonStartTime;
+            buttonCurrentTime += Time.deltaTime;
+            minButtonTime = Mathf.Max(minButtonTime, buttonCurrentTime - TimeController.Instance.rewindSeconds);
+        }
+        else if(TimeController.Instance.isRewindingTime)
+        {
+            //buttonCurrentTime = Mathf.Clamp(buttonCurrentTime - (Time.time - TimeController.Instance.rewindStartTime), 0f, buttonTimer);
+            //Debug.Log("minbuttontime = " + minButtonTime);
+            buttonCurrentTime = Mathf.Clamp(buttonCurrentTime - Time.deltaTime, minButtonTime, buttonTimer);
+        }
+    }
+
+    private void Update()
+    {
+        CalculateButtonCurrentTime();
+        //if(buttonCurrentTime != 0)
+            //Debug.Log(buttonCurrentTime);
     }
 }
