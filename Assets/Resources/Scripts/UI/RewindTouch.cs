f4ac97e237af6744e965072d2a8277a056b03c83
caioguedesam <caioguedesam@gmail.com>
2020-01-27 20:34:07 -0300

    Implementing rewind on touch outside UI - 27/01/2020

---
+++ Assets/Resources/Scripts/UI/RewindTouch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RewindTouch : MonoBehaviour
+{
+    PlayerControl player;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerControl>();
+    }
+
+    private void Update()
+    {
+        if(!EventSystem.current.IsPointerOverGameObject())
+        {
+            player.rewindInput = Input.GetMouseButton(0);
+            player.stoppedRewindInput = Input.GetMouseButtonUp(0);
+        }
+    }
+}
